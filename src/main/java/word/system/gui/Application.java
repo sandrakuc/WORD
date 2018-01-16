dd31f50edde9f8165614c2bc226b241bf2a063f2
Tomasz Kowalczyk <tomasz.kowalczyk94@gmail.com>
2018-01-16 12:52:58 +0100

    database

--- src/main/java/word/system/gui/Application.java
+++ src/main/java/word/system/gui/Application.java
@@ -1,10 +1,16 @@
 package word.system.gui;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import javax.sql.DataSource;
+
 @SpringBootApplication
 public class Application {
+
+    @Autowired
+    DataSource dataSource;
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
---
+++ src/main/java/word/system/gui/Customer.java
@@ -0,0 +1,52 @@
+package word.system.gui;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+public class Customer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "CUST_SEQ")
+    @SequenceGenerator(sequenceName = "customer_seq", allocationSize = 1, name = "CUST_SEQ")
+    Long id;
+
+    String name;
+
+    String email;
+
+    @Column(name = "CREATED_DATE")
+    Date date;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+}
