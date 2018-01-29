b53b587673a50d9423e0255a71f505dd1d150e32
wojtek <wojtek.klusek2@gmail.com>
2018-01-29 10:35:31 +0100

    createPkk done, new inserts

--- src/main/java/word/system/gui/viewsControllers/controllerCityDepEmployeePanel.java
+++ src/main/java/word/system/gui/viewsControllers/controllerCityDepEmployeePanel.java
@@ -1,16 +1,22 @@
 package word.system.gui.viewsControllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import word.system.user.User;
+import word.system.user.UserRepository;
 
 import javax.servlet.http.HttpServletRequest;
 
+
 @Controller
 public class controllerCityDepEmployeePanel {
+
+    @Autowired
+    UserRepository userRepository;
 
     //dostęp do tej strony po zalogowaniu jako admin, bedzie tu zarzadxzał bazą np dodawanie aut do bazy, egzaminatorow itp
     @RequestMapping("cityDepEmployeePanel")
@@ -34,12 +40,28 @@ public class controllerCityDepEmployeePanel {
 
 
     @GetMapping("createPkk")
-    public String formGet() {
+    public String formGet(Model model) {
+        //model.addAttribute("user", userRepository.getByLogin("admin"));
+
         return "userViews/actions/createPkk";
     }
 
     @PostMapping("createPkk")
-    public String formPost(User user, Model model) {
+    public String formPost(HttpServletRequest request, Model model) {
+
+        User user = new User();
+        user.setAddress(request.getParameter("address"));
+        user.setFirstName(request.getParameter("firstName"));
+        user.setLastName(request.getParameter("lastName"));
+        user.setAddress(request.getParameter("address"));
+        user.setEmail(request.getParameter("email"));
+        user.setRole(User.Role.PKK);
+        user.setLogin(request.getParameter("login"));
+        user.setPassword(request.getParameter("password"));
+
+        //System.out.println(user);
+        userRepository.save(user);
+
         model.addAttribute("user", user);
         return "userViews/actions/createPkk";
     }
--- src/main/resources/templates/userViews/cityDepEmployeePanel.ftl
+++ src/main/resources/templates/userViews/cityDepEmployeePanel.ftl
@@ -5,14 +5,14 @@
     		 <p>Wybierz co chcesz zrobić:</p><br>
 
     		
-    		 <form action="createPkk"  method="POST">
+    		 <form action="createPkk"  method="GET">
     				<p><b>Utworzyć Profil Kandydata na Kierowce</b></p>
     				<input name="createPkk" type="submit" value="Utwórz Pkk">
     			</form><br>
 
 
     		
-    		 <form action="prepereDriverLicenseApplication"  method="POST">
+    		 <form action="prepereDriverLicenseApplication"  method="GET">
     		       <p><b>Złóż dla zdającego wniosek o prawo jazdy</b></p><br>
                    	Wprowadz PESEL: <input type="text" name="pesel"><br><br>
                    	<input name="prepereApplication" type="submit" value="Zacznij wypełniać wniosek">
@@ -20,7 +20,7 @@
 
 
 
-             <form action="checkDriverLicenseApplication"  method="POST">
+             <form action="checkDriverLicenseApplication"  method="GET">
                 <p><b>Sprawdz status wniosku o prawo jazdy</b></p><br>
                  Wprowadz PESEL: <input type="text" name="pkk_pesel"><br><br>
                   <input name="checkStatus" type="submit" value="Sprawdz status wniosku">
@@ -28,7 +28,7 @@
 
     			 <br>
      			
-     			<form action="login"  method="POST">
+     			<form action="login"  method="GET">
      				 <input name="logout" type="submit" value="Wyloguj się">
      			 </form>
 </@t.page>
