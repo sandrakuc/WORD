864609c4e538f7df3a92b8a4a4e95a22cb246b49
sandrakuc <san.kuc.96@gmail.com>
2018-01-29 18:20:21 +0100

    repo

---
+++ src/main/java/word/system/exam/TeoreticalQuestions/AnswerRepository.java
@@ -0,0 +1,6 @@
+package word.system.exam.TeoreticalQuestions;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface AnswerRepository extends CrudRepository<AnswerBase, Long> {
+}
---
+++ src/main/java/word/system/exam/TeoreticalQuestions/QuestionRepository.java
@@ -0,0 +1,6 @@
+package word.system.exam.TeoreticalQuestions;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface QuestionRepository extends CrudRepository<QuestionBase, Long> {
+}
