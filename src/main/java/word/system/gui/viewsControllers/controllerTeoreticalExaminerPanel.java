7d3d21b356e60d6ea52057e0216f9e48718a6524
wojtek <wojtek.klusek2@gmail.com>
2018-01-31 22:34:18 +0100

    teoreticalExaminerPanel done

--- src/main/java/word/system/gui/viewsControllers/controllerTeoreticalExaminerPanel.java
+++ src/main/java/word/system/gui/viewsControllers/controllerTeoreticalExaminerPanel.java
@@ -8,11 +8,14 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import word.system.exam.TeoreticalExam;
 import word.system.exam.TeoreticalExamRepository;
+import word.system.exam.TeoreticalExamStatus;
 import word.system.user.User;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 import java.util.ArrayList;
+
+import static java.lang.Math.toIntExact;
 
 @Controller
 public class controllerTeoreticalExaminerPanel {
@@ -37,7 +40,7 @@ public class controllerTeoreticalExaminerPanel {
             }
         }
         System.out.println("\n\n\n" + teoreticalExams + "\n\n\n");
-        randQuestions();
+        modifyTable(request,model,teoreticalExams);
 
         model.addAttribute("userId", sessionUser.getId());
         model.addAttribute("teoreticalExams", teoreticalExams);
@@ -45,9 +48,56 @@ public class controllerTeoreticalExaminerPanel {
         return "userViews/teoreticalExaminerPanel";
     }
 
-    public void randQuestions()
-    {
 
+    private void modifyTable(HttpServletRequest request, Model model, ArrayList<TeoreticalExam> teoreticalExams)
+    {
+        String message = "";
+
+        //gdy user wprowadzi jakies id do pola tekstowego
+        if(request.getParameter("examId")!="" && request.getParameter("examId")!=null)
+        {
+            //pobieranie id egzaminu do zmiany i sprawdzanie czy taki istnieje na liscie dla tego egzaminatora egzaminow wtedy go zmieniamy
+            Integer examIdToChange = Integer.parseInt(request.getParameter("examId"));
+
+            Integer idInDatabase = examIdToChange;
+            Integer idInArrayList = checkIdEnterOnField(teoreticalExams,idInDatabase);
+            System.out.println("\n\n\n\n\n"+ idInArrayList + "\n\n\n\n");
+
+            if(checkIdEnterOnField(teoreticalExams,idInDatabase)!=-1)
+            {
+                //sprawdzanie co bylo zaznaczone w radioboxach
+                if(request.getParameter("examStatus").equals("otwarty"))
+                {
+                    teoreticalExams.get(idInArrayList).setTeoreticalExamStatus(TeoreticalExamStatus.TRWA);
+                }
+                else if(request.getParameter("examStatus").equals("zamkniety")) {
+                    teoreticalExams.get(idInArrayList).setTeoreticalExamStatus(TeoreticalExamStatus.ZAKONCZONY);
+                }
+                message = "Zmodyfikowano wpis";
+
+                teoreticalExamRepository.save(teoreticalExams.get(idInArrayList));
+                model.addAttribute("message",message);
+
+            }else
+                message = "Nie ma takiego id";
+
+
+        }
+        model.addAttribute("message",message);
+    }
+
+    private Integer checkIdEnterOnField(ArrayList<TeoreticalExam> teoreticalExams, Integer idInDatabase)
+    {
+        if(idInDatabase==null)
+            return -1;
+
+        for (TeoreticalExam p: teoreticalExams)
+        {
+            if(toIntExact( p.getId() ) == idInDatabase){
+                return teoreticalExams.indexOf(p);
+            }
+        }
+        return -1;
     }
 
     @GetMapping("teoreticalExaminerPanel")
